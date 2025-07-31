759110a9b1a7a87999ab8929a2cac8001485416f
Aaron <aarongardoni@gmail.com>
2025-08-01 09:29:31 +1000

    chore: lazy vim updates

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,10 +1,12 @@
 return {
+  -- { import = "nvchad.blink.lazyspec" },
   {
     "stevearc/conform.nvim",
     event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },                       -- These are some examples, uncomment them if you want to see them work!
   {
+
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
